c6826dd58876dc74e6d9789f39e5508dc4cdca3b
Samiran Roy <samiranroy0407@gmail.com>
2021-06-28 16:35:50 +0530

    CodeForces

---
+++ Palindromifier.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin>>str;
+	
+	int n = str.length();
+	
+	cout<<3<<"\n";
+	
+	cout<<"R"<<" "<<n-1<<"\n";
+	
+	cout<<"L"<<" "<<n<<"\n";
+	
+	cout<<"L"<<" "<<2;
+	
+	return 0;
+}
